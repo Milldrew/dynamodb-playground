87726d9250c5f95409b4a9f1b91e954afa1c1fc6
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-07-18 17:38:51 -0600

    📅  | Mon Jul 18 17:38:51 MDT 2022

---
+++ client.js
@@ -0,0 +1,14 @@
+// @ts-check
+import { DynamoDBClient, ListTablesCommand } from "aws-sdk/client-dynamodb";
+import fs from "fs";
+
+async () => {
+  const client = new DynamoDBClient({ region: "us-west-2" });
+  const command = new ListTablesCommand({});
+  try {
+    const results = await client.send(command);
+    console.log(results.TableNames.join("\n"));
+  } catch (err) {
+    console.error(err);
+  }
+};
